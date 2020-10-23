aff853c5b1ca96d80bb72a04271fad1f65161497
Nikoloz Chapichadze <nikoloz.chapichadze@gmail.com>
2020-10-23 08:13:03 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,15 +1,23 @@
 # require modules here
+<<<<<<< HEAD
 require 'pry'
 require "yaml"
 
 def load_library(file_path)
   emoticons = YAML.load_file(file_path)
+=======
+require "yaml"
+
+def load_library
+  emoticons = YAML.load_file('emoticons.yml')
+>>>>>>> dfa6a84d525487dc8dfcbc030ce99b1b704e9dee
   new_hash = emoticons.each_with_object({}) do |(key, element),new_hash|
     if(!new_hash[key])
       new_hash[key] = {}
     end
     
     if(!new_hash[key][:english])
+<<<<<<< HEAD
       new_hash[key][:english] = emoticons[key][0]
     end 
     
@@ -18,6 +26,16 @@ def load_library(file_path)
     end 
   
   end 
+=======
+      new_hash[key][:english] => emoticons[key][element][0]
+    end 
+    
+    if(!new_hash[key][:japanese])
+      new_hash[key][:japanese] => emoticons[key][element][1]
+    end 
+  end 
+  
+>>>>>>> dfa6a84d525487dc8dfcbc030ce99b1b704e9dee
   new_hash
 end
 
