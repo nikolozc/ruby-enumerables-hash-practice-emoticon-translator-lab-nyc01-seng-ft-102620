fd98aa3a45d240f1eab503fac291fce4b03173a3
Nikoloz Chapichadze <nikoloz.chapichadze@gmail.com>
2020-10-23 06:23:48 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,44 @@
 # require modules here
+require 'pry'
+require "yaml"
 
-def load_library
-  # code goes here
+def load_library(file_path)
+  emoticons = YAML.load_file(file_path)
+  new_hash = emoticons.each_with_object({}) do |(key, element),new_hash|
+    if(!new_hash[key])
+      new_hash[key] = {}
+    end
+    
+    if(!new_hash[key][:english])
+      new_hash[key][:english] = emoticons[key][0]
+    end 
+    
+    if(!new_hash[key][:japanese])
+      new_hash[key][:japanese] = emoticons[key][1]
+    end 
+  
+  end 
+  new_hash
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(file_path, western_emo)
+  emoticons = load_library(file_path)
+  sorry_message = "Sorry, that emoticon was not found"
+  emoticons.each do |key,element|
+    if(element.key(western_emo) != nil)
+      return emoticons[key][:japanese]
+    end 
+  end
+  return sorry_message
 end
 
-def get_english_meaning
-  # code goes here
+def get_english_meaning(file_path, japanese_emo)
+  emoticons = load_library(file_path)
+  sorry_message = "Sorry, that emoticon was not found"
+  emoticons.each do |key,element|
+    if(element.key(japanese_emo) != nil)
+      return key
+    end 
+  end
+  return sorry_message
 end
